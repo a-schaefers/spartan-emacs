0126fb9d535047c4b35fb1e0a5b21b1f65032e1e
Adam Schaefers <schaefers@riseup.net>
2021-02-18 18:03:33 -0800

    Make shell-mode bind less conflicting

--- init.el
+++ init.el
@@ -230,7 +230,7 @@
 (setq flymake-shellcheck-use-file nil)
 (add-hook 'sh-mode-hook 'flymake-shellcheck-load)
 
-(global-set-key (kbd "C-<return>") 'better-shell-for-current-dir)
+(global-set-key (kbd "C-c $") 'better-shell-for-current-dir)
 
 ;; PYTHON
 
