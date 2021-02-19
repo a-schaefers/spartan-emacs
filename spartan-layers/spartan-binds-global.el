3d940953e847ece35a7fb666bf8e2a9c4843c52e
Adam Schaefers <schaefers@riseup.net>
2021-02-19 13:56:17 -0800

    Make binds more modular

--- spartan-layers/spartan-binds-global.el
+++ spartan-layers/spartan-binds-global.el
@@ -4,29 +4,30 @@
 (global-unset-key (kbd "C-x C-z"))
 (global-unset-key (kbd "M-m"))
 
+(with-eval-after-load 'spartan-collect-defun
+  (global-set-key (kbd "C-%") 'forward-or-backward-sexp)) ; jump to matching bracket ala vim
 
-(global-set-key (kbd "C-c i") #'(lambda ()
+(with-eval-after-load 'crux
+  (global-set-key (kbd "C-a") 'crux-move-beginning-of-line)
+  (global-set-key (kbd "C-o") 'crux-smart-open-line)
+  (global-set-key (kbd "C-c C-l") 'crux-duplicate-current-line-or-region)
+  (global-set-key (kbd "C-c C--") 'crux-kill-whole-line)
+  (global-set-key (kbd "C-c ;") 'crux-duplicate-and-comment-current-line-or-region))
+
+(with-eval-after-load 'browse-kill-ring
+  (global-set-key (kbd "M-y") 'browse-kill-ring))
+
+(with-eval-after-load 'spartan-collect-defun
+  (global-set-key (kbd "C-x C-o") 'spacemacs/alternate-window)
+  (global-set-key (kbd "C-x C-b") 'spacemacs/alternate-buffer))
+
+(with-eval-after-load 'spartan-collect-defun
+  (global-set-key (kbd "C-=") #'(lambda ()
                                   (interactive)
-                                  (find-file user-init-file)))
-
-(global-set-key (kbd "C-%") 'forward-or-backward-sexp) ; jump to matching bracket ala vim
-(global-set-key (kbd "C-a") 'crux-move-beginning-of-line)
-(global-set-key (kbd "C-o") 'crux-smart-open-line)
-(global-set-key (kbd "C-c C-l") 'crux-duplicate-current-line-or-region)
-(global-set-key (kbd "C-c C--") 'crux-kill-whole-line)
-(global-set-key (kbd "C-c ;") 'crux-duplicate-and-comment-current-line-or-region)
-(global-set-key (kbd "M-y") 'browse-kill-ring)
-
-(global-set-key (kbd "C-x C-o") 'spacemacs/alternate-window)
-(global-set-key (kbd "C-x C-b") 'spacemacs/alternate-buffer)
-(global-set-key (kbd "C-x <C-backspace>") 'bury-buffer)
-
-(global-set-key (kbd "C-=") #'(lambda ()
-                     (interactive)
-                     (spartan-font-resizer 1)))
-(global-set-key (kbd "C--") #'(lambda ()
-                     (interactive)
-                     (spartan-font-resizer -1)))
+                                  (spartan-font-resizer 1)))
+  (global-set-key (kbd "C--") #'(lambda ()
+                                  (interactive)
+                                  (spartan-font-resizer -1))))
 
 ;; REGEXP search
 
@@ -57,7 +58,8 @@
 
 ;; PASTEBIN
 
-(global-set-key (kbd "C-c t r") 'region-to-termbin)
-(global-set-key (kbd "C-c t b") 'buffer-to-termbin)
+(with-eval-after-load 'spartan-collect-defun
+  (global-set-key (kbd "C-c t r") 'region-to-termbin)
+  (global-set-key (kbd "C-c t b") 'buffer-to-termbin))
 
 (provide 'spartan-binds-global)
