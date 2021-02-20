7a495d7f4b9de97e9bbb0b5d361baa8d53a6417b
Adam Schaefers <schaefers@riseup.net>
2021-02-20 10:45:19 -0800

    Slightly better elisp

--- init.el
+++ init.el
@@ -71,7 +71,7 @@
 
 ;; M-x customize
 
-(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load-file custom-file))
 
