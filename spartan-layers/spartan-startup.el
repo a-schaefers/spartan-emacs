e7cfef9f79443e6e7764b9f3f6e5eb5bc125c398
Adam Schaefers <schaefers@riseup.net>
2021-02-23 08:21:26 -0800

    spartan-startup uses after-init-hook, not emacs-startup-hook

--- spartan-layers/spartan-startup.el
+++ spartan-layers/spartan-startup.el
@@ -4,16 +4,16 @@
       initial-major-mode 'emacs-lisp-mode
       load-prefer-newer t)
 
-(add-hook 'emacs-startup-hook #'(lambda ()
-				  (interactive)
+(add-hook 'after-init-hook #'(lambda ()
+			       (interactive)
 
-				  (require 'server)
-				  (or (server-running-p)
-				      (server-start))
+			       (require 'server)
+			       (or (server-running-p)
+				   (server-start))
 
-				  (message (concat "Welcome "
-						   user-login-name
-						   ", this is Sparta!"
-						   " {}oo((X))ΞΞΞΞΞΞΞΞΞΞΞΞΞ>"))))
+			       (message (concat "Welcome "
+						user-login-name
+						", this is Sparta!"
+						" {}oo((X))ΞΞΞΞΞΞΞΞΞΞΞΞΞ>"))))
 
 (provide 'spartan-startup)
