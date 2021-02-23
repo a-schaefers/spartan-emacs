ec80bfd871dae7aa9d46c57fb0f7ae2abc7a4150
Adam Schaefers <schaefers@riseup.net>
2021-02-23 08:20:50 -0800

    Default Emacs startup screen, for now...

--- spartan-layers/spartan-startup.el
+++ spartan-layers/spartan-startup.el
@@ -1,21 +1,19 @@
 ;;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-(setq inhibit-startup-screen t
+(setq inhibit-startup-screen nil
       initial-major-mode 'emacs-lisp-mode
       load-prefer-newer t)
 
 (add-hook 'emacs-startup-hook #'(lambda ()
-                                  (interactive)
+				  (interactive)
 
-                                  (require 'server)
-                                  (or (server-running-p)
-                                      (server-start))
+				  (require 'server)
+				  (or (server-running-p)
+				      (server-start))
 
-                                  (find-file user-init-file)
-
-                                  (message (concat "Welcome "
-                                                   user-login-name
-                                                   ", this is Sparta!"
-                                                   " {}oo((X))ΞΞΞΞΞΞΞΞΞΞΞΞΞ>"))))
+				  (message (concat "Welcome "
+						   user-login-name
+						   ", this is Sparta!"
+						   " {}oo((X))ΞΞΞΞΞΞΞΞΞΞΞΞΞ>"))))
 
 (provide 'spartan-startup)
