e5d4076378e3d8ddd737450c805a555f11bc86c1
Adam Schaefers <schaefers@riseup.net>
2021-02-22 17:25:21 -0800

    Add binds for vterm

--- spartan-layers/spartan-binds-global.el
+++ spartan-layers/spartan-binds-global.el
@@ -1,16 +1,32 @@
 ;;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-;; Unbind annoyances
+;; UNBIND ANNOYANCES
+
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "C-x C-z"))
 (global-unset-key (kbd "M-m"))
 
-;; Tab auto completion
+;; TAB AUTO COMPLETION
 (setq tab-always-indent 'complete) ; used by eglot for auto-completion as well
 
-;; Better-defaults
+;; BETTER-DEFAULTS
+
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-/") 'hippie-expand)
+
+;; REDICULOUSLY USEFUL TRAMP COMMAND
+
+(defun spartan-tramp (x)
+  "Tramp ssh to a server, optionally as root, and [optionally] store creds in .authinfo[.gpg] if Emacs 27"
+  (interactive "sServer name: ")
+  (if (yes-or-no-p "sudo to root? ")
+    (find-file (concat "/ssh:" x "|sudo:" x ":"))
+  (find-file (concat "/ssh:" x ":"))))
+
+(defalias 'tramp 'spartan-tramp)
+(global-set-key (kbd "C-c t") 'spartan-tramp)
+
+;; MATCHING BRACKET LIKE VIM's "%"
 
 (defun forward-or-backward-sexp (&optional arg)
   "Go to the matching parenthesis character if one is adjacent to point."
@@ -21,22 +37,9 @@
 	((looking-at "\\s)") (forward-char) (backward-sexp arg))
 	((looking-back "\\s(" 1) (backward-char) (forward-sexp arg))))
 
-;; tramp
-
-(defun spartan-tramp (x)
-  "Tramp ssh to a server, optionally as root, and [optionally] store creds in .authinfo[.gpg]"
-  (interactive "sServer name: ")
-  (if (yes-or-no-p "sudo to root? ")
-    (find-file (concat "/ssh:" x "|sudo:" x ":"))
-  (find-file (concat "/ssh:" x ":"))))
-
-(defalias 'tramp 'spartan-tramp)
-(global-set-key (kbd "C-c t") 'spartan-tramp)
-
-;; Ala vim
 (global-set-key (kbd "C-%") 'forward-or-backward-sexp)
 
-;; Collection of Rediculously useful eXtensions (requires spartan-crux layer)
+;; COLLECTION OF REDICULOUSLY USEFUL EXTENSIONS
 
 (with-eval-after-load 'crux
   (global-set-key (kbd "C-a") 'crux-move-beginning-of-line)
@@ -45,13 +48,13 @@
   (global-set-key (kbd "C-c C--") 'crux-kill-whole-line)
   (global-set-key (kbd "C-c ;") 'crux-duplicate-and-comment-current-line-or-region))
 
-;; Browse Kill Ring (requires spartan-kill-ring layer)
+;; BROWSE KILL RING
 
 (with-eval-after-load 'browse-kill-ring
   (defalias 'kr 'browse-kill-ring)
   (global-set-key (kbd "M-y") 'browse-kill-ring))
 
-;; Global font size adjustments (requires spartan-better-defaults layer)
+;; GLOBAL FONT SIZE ADJUSTMENTS
 
 (with-eval-after-load 'spartan-better-defaults
   (global-set-key (kbd "C-=") #'(lambda ()
@@ -61,7 +64,7 @@
 				  (interactive)
 				  (spartan-font-resizer -1))))
 
-;; REGEXP search
+;; REGEXP SEARCH
 
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
@@ -81,36 +84,43 @@
 (defalias 'ed 'ediff)
 (global-set-key (kbd "C-c |") 'ediff)
 
-;; GIT (requires spartan-magit layer)
+;; GIT
 
 (with-eval-after-load 'magit
   (defalias 'git 'magit)
   (global-set-key (kbd "C-c g") 'magit))
 
-;; LINTER  (requires spartan-flymake layer)
+;; LINTER
 
 (with-eval-after-load 'spartan-flymake
   (defalias 'lint 'flymake-show-diagnostics-buffer)
   (global-set-key (kbd "C-c f") 'flymake-show-diagnostics-buffer))
 
-;; Pastebin (requires spartan-webpaste layer)
+;; PASTEBIN
 
 (with-eval-after-load 'spartan-webpaste
   (defalias 'pb 'webpaste-paste-buffer-or-region)
   (global-set-key (kbd "C-c C-p C-p") 'webpaste-paste-buffer-or-region))
 
-;; DUMB TERM (requires spartan-shell layer)
+;; DUMB TERM
 
 (with-eval-after-load 'spartan-shell
   (defalias 'sh 'better-shell-for-current-dir)
   (global-set-key (kbd "C-c $") 'better-shell-for-current-dir))
+
+;; VTERM
+
+(with-eval-after-load 'spartan-vterm
+  (defalias 'vt 'vterm)
+  (global-set-key (kbd "C-c C-v") 'vterm)
+  (global-set-key (kbd "C-c v") 'vterm))
 
 ;; COMPILE COMMAND
 
 (setq compile-command "make -k ")
 (global-set-key (kbd "<f5>") 'compile)
 
-;; EXECUTE SCRIPT (requires spartan-shell layer)
+;; EXECUTE SCRIPT
 
 (with-eval-after-load 'spartan-shell
   (global-set-key (kbd "<f6>") 'spartan-script-execute))
