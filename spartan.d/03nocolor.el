cc4dfdefe244ad53c1c57c4d565f9da4e5da14a3
Adam Schaefers <schaefers@riseup.net>
2021-02-23 23:54:35 -0800

    Color overrides

---
+++ spartan.d/03nocolor.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t; no-byte-compile: t; -*-
+
+(global-font-lock-mode 1)
+
+(dolist (f (face-list))
+  (progn
+    (set-face-foreground f nil)
+    (set-face-background f nil)))
+
+(dolist (f '(highlight
+	     isearch
+	     query-replace
+	     lazy-highlight
+	     region
+	     secondary-selection
+	     trailing-whitespace))
+  (set-face-background f "gray97"))
