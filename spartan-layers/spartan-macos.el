2bd47dc5432936f595f8c3fa68f7ccf3445ba0b0
Adam Schaefers <schaefers@riseup.net>
2021-02-24 10:20:33 -0800

    macos clipboard terminal support

--- spartan-layers/spartan-macos.el
+++ spartan-layers/spartan-macos.el
@@ -6,8 +6,15 @@
   (exec-path-from-shell-initialize))
 
 (when (or (string= (getenv "XDG_CURRENT_DESKTOP") "gnome")
-          (string= system-type "darwin"))
+	  (string= system-type "darwin"))
   (add-to-list 'spartan-package-list 'exec-path-from-shell)
   (add-hook 'after-init-hook 'spartan-macos-hook))
+
+;; clipboard support in a terminal
+(and (string= system-type "darwin")
+     (or window-system
+	 (progn
+	   (add-to-list 'spartan-package-list 'osx-clipboard)
+	   (osx-clipboard-mode +1))))
 
 (provide 'spartan-macos)
