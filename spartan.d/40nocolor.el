57104609fade0eb1f002767c40e3468e1b878b5e
Adam Schaefers <schaefers@riseup.net>
2021-02-24 10:04:23 -0800

    Improved nocolor for terminals -- great with transparent terminal backgrounds

--- spartan.d/40nocolor.el
+++ spartan.d/40nocolor.el
@@ -2,16 +2,21 @@
 
 (global-font-lock-mode -1)
 
-(dolist (f (face-list))
-  (progn
-    (set-face-foreground f nil)
-    (set-face-background f nil)))
+(and window-system
+     (progn
+       (dolist (f (face-list))
+	 (progn
+	   (set-face-foreground f nil)
+	   (set-face-background f nil)))
 
-(dolist (f '(highlight
-	     isearch
-	     query-replace
-	     lazy-highlight
-	     region
-	     secondary-selection
-	     trailing-whitespace))
-  (set-face-background f "gray97"))
+       (dolist (f '(highlight
+		    isearch
+		    query-replace
+		    lazy-highlight
+		    region
+		    secondary-selection
+		    trailing-whitespace))
+	 (set-face-background f "gray97"))))
+
+(or window-system
+    (add-to-list 'default-frame-alist '(tty-color-mode . -1)))
