c89c2256e512e3c71bc6baa062d7d39b3d74afa1
Adam Schaefers <schaefers@riseup.net>
2021-02-24 15:13:47 -0800

    Add terminal paste-mode

--- spartan-layers/spartan-emacs-nw.el
+++ spartan-layers/spartan-emacs-nw.el
@@ -2,6 +2,20 @@
 
 ;; for terminal Emacs only (emacs -nw)
 
+(defun spartan-paste-mode ()
+  "Experimental. This may or may not work. For pasting in to terminal Emacs. Anyone have a better idea?"
+  (interactive)
+  (if (bound-and-true-p spartan-paste-on)
+    (progn
+      (funcall spartan-paste-stored-mode)
+      (setq spartan-paste-on nil)
+      (message "PASTE MODE OFF"))
+  (progn
+    (setq spartan-paste-stored-mode major-mode
+	  spartan-paste-on t)
+    (fundamental-mode)
+    (message "PASTE MODE ON"))))
+
 (defun spartan-emacs-nw-hook ()
 
   (global-set-key (kbd "C-x ;") 'comment-line) ; "C-x C-;" is interpreted this way in some terminals
@@ -9,7 +23,9 @@
   (xterm-mouse-mode 1)
 
   (require 'clipetty)
-  (global-clipetty-mode +1))
+  (global-clipetty-mode +1)
+
+  (global-set-key (kbd "<f7>") 'spartan-paste-mode))
 
 (or window-system
     (progn
