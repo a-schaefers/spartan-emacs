4a8bd53572ffeba51945e3e6ec2079f7231b6067
Adam Schaefers <schaefers@riseup.net>
2021-02-25 10:45:14 -0800

    Make disablement of syntax highlighting optional, add elegant fixes

---
+++ spartan.d/50nocolor.el
@@ -0,0 +1,9 @@
+;;; -*- lexical-binding: t; no-byte-compile: t; -*-
+
+;; disable syntax highlighting
+;; (global-font-lock-mode -1)
+
+;; disable all colors (only works for terminal Emacs)
+;; if you find a way to do this in GUI, let me know please...
+;; (or window-system
+;;     (add-to-list 'default-frame-alist '(tty-color-mode . -1)))
