33a9b7d60a8a405050e43a3cf793101bf8e727d7
Adam Schaefers <schaefers@riseup.net>
2021-03-08 12:00:23 -0800

    Add comments, simplify

--- spartan-layers/spartan-emacs-nw.el
+++ spartan-layers/spartan-emacs-nw.el
@@ -2,30 +2,13 @@
 
 ;; for terminal Emacs only (emacs -nw)
 
-(defun spartan-paste-mode ()
-  "Experimental. This may or may not work. For pasting in to terminal Emacs. Anyone have a better idea?"
-  (interactive)
-  (if (bound-and-true-p spartan-paste-on)
-    (progn
-      (funcall spartan-paste-stored-mode)
-      (setq spartan-paste-on nil)
-      (message "PASTE MODE OFF"))
-  (progn
-    (setq spartan-paste-stored-mode major-mode
-	  spartan-paste-on t)
-    (fundamental-mode)
-    (message "PASTE MODE ON"))))
-
 (defun spartan-emacs-nw-hook ()
-
   (global-set-key (kbd "C-x ;") 'comment-line) ; "C-x C-;" is interpreted this way in some terminals
 
-  (xterm-mouse-mode 1)
+  (xterm-mouse-mode 1) ; mouse support
 
-  (require 'clipetty)
-  (global-clipetty-mode +1)
-
-  (global-set-key (kbd "<f7>") 'spartan-paste-mode))
+  (require 'clipetty) ; clipboard support
+  (global-clipetty-mode +1))
 
 (or window-system
     (progn
