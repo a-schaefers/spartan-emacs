e006298f5e0db05632b50ac184d736fc730cf50d
Adam Schaefers <schaefers@riseup.net>
2021-03-11 13:17:36 -0800

    [Perf] Remove better-flx for ido layer for better performance

--- spartan-layers/spartan-ido.el
+++ spartan-layers/spartan-ido.el
@@ -2,19 +2,17 @@
 
 (add-to-list 'spartan-package-list 'amx)
 (add-to-list 'spartan-package-list 'ido-completing-read+)
-(add-to-list 'spartan-package-list 'flx-ido)
 
 (defun spartan-ido-hook ()
   (require 'ido)
   (require 'ido-completing-read+)
   (require 'amx)
-  (require 'flx-ido)
 
   (setq ido-create-new-buffer 'always
-	ido-auto-merge-work-directories-length -1
-	ido-enable-flex-matching t
-	ido-use-filename-at-point 'guess
-	ido-use-faces nil)
+        ido-auto-merge-work-directories-length -1
+        ido-enable-flex-matching t
+        ido-use-filename-at-point 'guess
+        ido-use-faces nil)
 
   (ido-mode 1)
   (ido-everywhere 1)
@@ -24,14 +22,8 @@
 
   (with-eval-after-load 'amx
     (setq amx-ignored-command-matchers nil
-	  amx-show-key-bindings nil)
-    (amx-mode 1))
-
-  (with-eval-after-load 'flx-ido
-    (flx-ido-mode 1)
-    (setq ido-enable-flex-matching t)
-    (setq ido-use-faces nil)
-    (setq flx-ido-threshold '1000)))
+          amx-show-key-bindings nil)
+    (amx-mode 1)))
 
 (add-hook 'after-init-hook 'spartan-ido-hook)
 
