3806c66f22ade364da444841e61804956dc0cc8f
jmj <jmjcat@riseup.net>
2022-02-13 21:00:53 -0800

    [Feature] Support CL with slime, Add clojure-mode and lsp support

--- spartan-layers/spartan-lisp.el
+++ spartan-layers/spartan-lisp.el
@@ -1,7 +1,10 @@
 ;;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
 (add-to-list 'spartan-package-list 'paredit)
+(add-to-list 'spartan-package-list 'slime)
+(add-to-list 'spartan-package-list 'clojure-mode)
 
+;; paredit everywhere
 (defun spartan-lisp-hook ()
   (require 'paredit)
   (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
@@ -9,7 +12,19 @@
   (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
   (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
+
+;; Common Lisp
+(setq inferior-lisp-program "sbcl")
+(add-to-list 'auto-mode-alist '("\\.sbclrc\\'" . lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.cl\\'" . lisp-mode))
+
+;; Clojure
+(when (or (executable-find "clojure-lsp")
+          (executable-find "clojure-lsp-bin"))
+        (with-eval-after-load 'eglot
+          (add-hook 'clojure-mode-hook 'eglot-ensure)))
 
 (add-hook 'after-init-hook 'spartan-lisp-hook)
 
