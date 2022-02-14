adbdb371e3151ef935ee96ea509d39e211552a89
jmj <jmjcat@riseup.net>
2022-02-14 09:46:44 -0800

    [Feature] Add clojure repl & lsp

--- spartan-layers/spartan-lisp.el
+++ spartan-layers/spartan-lisp.el
@@ -3,6 +3,11 @@
 (add-to-list 'spartan-package-list 'paredit)
 (add-to-list 'spartan-package-list 'slime)
 (add-to-list 'spartan-package-list 'clojure-mode)
+(add-to-list 'spartan-package-list 'inf-clojure)
+
+(setq inferior-lisp-program "sbcl")
+(add-to-list 'auto-mode-alist '("\\.sbclrc\\'" . lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.cl\\'" . lisp-mode))
 
 ;; paredit everywhere
 (defun spartan-lisp-hook ()
@@ -13,18 +18,21 @@
   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
   (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
+  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
-;; Common Lisp
-(setq inferior-lisp-program "sbcl")
-(add-to-list 'auto-mode-alist '("\\.sbclrc\\'" . lisp-mode))
-(add-to-list 'auto-mode-alist '("\\.cl\\'" . lisp-mode))
+  ;; Clojure
+  (when (executable-find "clojure-lsp")
+    (with-eval-after-load 'eglot
+      (add-hook 'clojure-mode-hook 'eglot-ensure)))
 
-;; Clojure
-(when (or (executable-find "clojure-lsp")
-          (executable-find "clojure-lsp-bin"))
-        (with-eval-after-load 'eglot
-          (add-hook 'clojure-mode-hook 'eglot-ensure)))
+  (with-eval-after-load 'clojure
+    (define-key clojure-mode-map (kbd "M-m rr") 'inf-clojure-eval-region)
+    (define-key clojure-mode-map (kbd "M-m rb") 'inf-clojure-eval-buffer)
+    (define-key clojure-mode-map (kbd "M-m rR") 'inf-clojure))
+
+  (with-eval-after-load 'eglot
+      (add-to-list 'eglot-server-programs
+                   `(clojure-mode . ("clojure-lsp")))))
 
 (add-hook 'after-init-hook 'spartan-lisp-hook)
 
