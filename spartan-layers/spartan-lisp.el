e5dab32b1d05aef292dfce99ba75154e46cf7733
jmj <jmjcat@riseup.net>
2022-02-14 10:04:57 -0800

    [Feature] Add racket

--- spartan-layers/spartan-lisp.el
+++ spartan-layers/spartan-lisp.el
@@ -4,6 +4,7 @@
 (add-to-list 'spartan-package-list 'slime)
 (add-to-list 'spartan-package-list 'clojure-mode)
 (add-to-list 'spartan-package-list 'inf-clojure)
+(add-to-list 'spartan-package-list 'racket-mode)
 
 (setq inferior-lisp-program "sbcl")
 (add-to-list 'auto-mode-alist '("\\.sbclrc\\'" . lisp-mode))
@@ -19,20 +20,30 @@
   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
   (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
   (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+  (add-hook 'racket-mode-hook           #'enable-paredit-mode)
+
+  ;;Racket
+  (add-hook 'racket-mode-hook
+          (lambda ()
+            ;; Likewise for racket-repl-mode-hook and racket-repl-mode-map.
+            (define-key racket-mode-map (kbd "<f5>") 'racket-run)
+            (define-key racket-mode-map (kbd "M-m rR") 'racket-repl)
+            (define-key racket-mode-map (kbd "M-m rr") 'racket-repl-submit)))
 
   ;; Clojure
+  (with-eval-after-load 'eglot
+      (add-to-list 'eglot-server-programs
+                   `(clojure-mode . ("clojure-lsp"))))
+
   (when (executable-find "clojure-lsp")
     (with-eval-after-load 'eglot
       (add-hook 'clojure-mode-hook 'eglot-ensure)))
 
-  (with-eval-after-load 'clojure
-    (define-key clojure-mode-map (kbd "M-m rr") 'inf-clojure-eval-region)
-    (define-key clojure-mode-map (kbd "M-m rb") 'inf-clojure-eval-buffer)
-    (define-key clojure-mode-map (kbd "M-m rR") 'inf-clojure))
-
-  (with-eval-after-load 'eglot
-      (add-to-list 'eglot-server-programs
-                   `(clojure-mode . ("clojure-lsp")))))
+  (add-hook 'clojure-mode-hook
+            (lambda ()
+              (define-key clojure-mode-map (kbd "M-m rr") 'inf-clojure-eval-region)
+              (define-key clojure-mode-map (kbd "M-m rb") 'inf-clojure-eval-buffer)
+              (define-key clojure-mode-map (kbd "M-m rR") 'inf-clojure))))
 
 (add-hook 'after-init-hook 'spartan-lisp-hook)
 
