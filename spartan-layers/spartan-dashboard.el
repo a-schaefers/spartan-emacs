59f073f7ff08a276ae8109262e8c5ece09c1ffa1
Adam Schaefers <schaefers@riseup.net>
2023-09-02 23:47:48 -0600

    Improve startup screen, Add recent screenshot to README

--- spartan-layers/spartan-dashboard.el
+++ spartan-layers/spartan-dashboard.el
@@ -6,13 +6,16 @@
   :config
   (dashboard-setup-startup-hook))
 
-(setq dashboard-startup-banner nil)
-(setq dashboard-banner-logo-title "
- _______  _____  _______  ______ _______ _______
- |______ |_____] |_____| |_____/    |    |_____|
- ______| |       |     | |    \\_    |    |     |
-")
-
+(setq dashboard-startup-banner (concat user-emacs-directory "spartan-army-spear-royalty-free-spear.png"))
+(setq dashboard-center-content t)
+(setq dashboard-banner-logo-title "Spartan Emacs")
 (setq dashboard-footer-messages '("Vincit qui se vincit."))
+
+(setq dashboard-items '((recents  . 5)
+                        ;; (bookmarks . 5)
+                        ;; (projects . 5)
+                        ;; (agenda . 5)
+                        ;; (registers . 5)
+                        ))
 
 (provide 'spartan-dashboard)
