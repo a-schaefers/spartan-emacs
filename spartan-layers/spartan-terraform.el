9554211df816ddafee6ad117cd7199c69b9e67a6
Adam Schaefers <schaefers@riseup.net>
2023-09-04 15:10:07 -0600

    [spartan-terraform.el] Add LSP support

--- spartan-layers/spartan-terraform.el
+++ spartan-layers/spartan-terraform.el
@@ -8,6 +8,12 @@
   :demand t
   :config
   (custom-set-variables
-   '(terraform-indent-level 2)))
+   '(terraform-indent-level 2))
+  (with-eval-after-load 'eglot
+      (add-to-list 'eglot-server-programs
+                   `(terraform-mode . ("terraform-ls" "serve"))))
+  (when (executable-find "terraform-ls")
+        (with-eval-after-load 'eglot
+          (add-hook 'terraform-mode-hook 'eglot-ensure))))
 
 (provide 'spartan-terraform)
