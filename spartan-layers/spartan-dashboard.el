2624715823cdea80ce31ebb5279d733409a7fb3c
Adam Schaefers <schaefers@riseup.net>
2023-09-04 14:06:42 -0600

    Add specific startup message only for the first run of spartan Emacs

--- spartan-layers/spartan-dashboard.el
+++ spartan-layers/spartan-dashboard.el
@@ -9,8 +9,14 @@
 (setq dashboard-startup-banner (concat user-emacs-directory "spartan-army-spear-royalty-free-spear.png"))
 (setq dashboard-center-content t)
 (setq dashboard-banner-logo-title "Spartan Emacs")
-;; (setq dashboard-footer-messages '("Vincit qui se vincit."))
-(setq dashboard-footer-messages '("Tip: Use 'C-c i' to view settings pertaining to Spartan Emacs configuration"))
+
+(setq dashboard-footer-messages '("Vincit qui se vincit."))
+
+(or (file-exists-p (concat user-emacs-directory ".spartan-first-run"))
+    (progn
+      (setq dashboard-footer-messages '("Tip: Use 'C-c i' to view settings pertaining to Spartan Emacs configuration"))
+      (with-temp-file (concat user-emacs-directory ".spartan-first-run") (insert "Hello World"))))
+
 
 (setq dashboard-items '((recents  . 5)
                         ;; (bookmarks . 5)
