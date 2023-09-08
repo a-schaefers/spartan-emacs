57a15ba99101f36dca0a2e871bc86f720bf62e29
Adam Schaefers <schaefers@riseup.net>
2023-09-08 17:38:15 -0600

    Add consult

--- spartan-layers/spartan-flymake.el
+++ spartan-layers/spartan-flymake.el
@@ -5,12 +5,14 @@
   :demand t
   :config
   (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
+  (add-hook 'prog-mode-hook 'flymake-mode)
 
-  (defun spartan-lint ()
-    (interactive)
-    (flymake-mode 1)
-    (flymake-show-diagnostics-buffer))
+  ;; (defun spartan-lint ()
+  ;;   (interactive)
+  ;;   (flymake-mode 1)
+  ;;   (flymake-show-diagnostics-buffer))
 
-  (defalias 'lint 'spartan-lint))
+  ;; (defalias 'lint 'spartan-lint)
+  )
 
 (provide 'spartan-flymake)
