f848e6f1a9131c76b781ca1996a3bf4e1243871c
Adam Schaefers <schaefers@riseup.net>
2023-09-17 11:53:09 -0600

    [spartan-eglot] Fix bind keys

--- spartan-layers/spartan-eglot.el
+++ spartan-layers/spartan-eglot.el
@@ -8,14 +8,14 @@
 (use-package eglot
   :after yasnippet
   :straight t
-  :defer t
-  :config
-  (define-key eglot-mode-map (kbd "M-m dd") 'eldoc)
-  (define-key eglot-mode-map (kbd "M-m ,") 'eglot-rename)
-  (define-key eglot-mode-map (kbd "M-m o") 'eglot-code-action-organize-imports)
-  (define-key eglot-mode-map (kbd "M-=") 'eglot-format)
-  (define-key eglot-mode-map (kbd "M-?") 'xref-find-references)
-  (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions)
-  (define-key eglot-mode-map (kbd "M-/") 'completion-at-point))
+  :defer t)
+
+(with-eval-after-load 'eglot
+          (define-key eglot-mode-map (kbd "M-m r") 'eglot-rename)
+          (define-key eglot-mode-map (kbd "M-m o") 'eglot-code-action-organize-imports)
+          (define-key eglot-mode-map (kbd "M-m h") 'eldoc)
+          (define-key eglot-mode-map (kbd "M-m =") 'eglot-format)
+          (define-key eglot-mode-map (kbd "M-m ?") 'xref-find-references)
+          (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions))
 
 (provide 'spartan-eglot)
