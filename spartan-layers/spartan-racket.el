60bdcfe441eef7d5f25d8047779d3b8b316148ed
Adam Schaefers <schaefers@riseup.net>
2023-09-20 20:36:25 -0600

    Add lsp for for racket

--- spartan-layers/spartan-racket.el
+++ spartan-layers/spartan-racket.el
@@ -2,10 +2,11 @@
 
 (use-package racket-mode
   :straight t
-  :defer t
-  ;; :init
-  )
+  :defer t)
 
-;; todo add lsp
+;; hmm no quick way shown to detect path of this one https://github.com/jeapostrophe/racket-langserver
+;; add to spartan.el (setq spartan-racket-langserver t)
+(when (bound-and-true-p spartan-racket-langserver)
+  (add-hook 'racket-mode-hook 'eglot-ensure))
 
 (provide 'spartan-racket)
