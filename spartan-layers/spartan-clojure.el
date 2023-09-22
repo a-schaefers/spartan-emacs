e13a29a8c2c6bbd08f3f599d288b85f0671bcd0d
Adam Schaefers <schaefers@riseup.net>
2023-09-21 20:53:29 -0600

    [spartan-clojure] clojure-lsp freezes eglot in my testing, use cider

--- spartan-layers/spartan-clojure.el
+++ spartan-layers/spartan-clojure.el
@@ -1,33 +1,12 @@
 ;;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-;; requires spartan-elisp
-
 (use-package clojure-mode
   :straight t
   :defer t
-  :init
-
-  (when (executable-find "clojure-lsp")
-    (add-hook 'clojure-mode-hook 'eglot-ensure))
-
-  :config
-  (require 'eglot)
-  (add-to-list 'eglot-server-programs
-                   `(clojure-mode . ("clojure-lsp")))
-  (with-eval-after-load 'eglot
-    (setq eglot-connect-timeout 60)))
+  :config)
 
 (use-package cider
   :straight t
-  :defer t
-  :init
-  ;; https://manueluberti.eu/2023/03/25/clojure-lsp.html
-  (setq-default cider-eldoc-display-for-symbol-at-point nil)
-
-  (defun mu-cider-disable-eldoc ()
-    "Let LSP handle ElDoc instead of CIDER."
-    (remove-hook 'eldoc-documentation-functions #'cider-eldoc t))
-
-  (add-hook 'cider-mode-hook #'mu-cider-disable-eldoc))
+  :defer t)
 
 (provide 'spartan-clojure)
