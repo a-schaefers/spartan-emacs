fae3f9f445413e811b184456c9e418a984774bc9
Adam Schaefers <schaefers@riseup.net>
2023-09-23 13:53:38 -0600

    Fix C indent tabs to be buffer local

--- spartan-layers/spartan-c.el
+++ spartan-layers/spartan-c.el
@@ -7,10 +7,11 @@
 ;; https://www.kernel.org/doc/html/v4.10/process/coding-style.html#linux-kernel-coding-style
 (setq c-default-style "linux"
       c-basic-offset 8)
+
 (add-hook 'makefile-mode-hook (lambda ()
-                                (setq indent-tabs-mode t)))
+                                (setq-local indent-tabs-mode t)))
 (add-hook 'c-mode-hook (lambda ()
-                                (setq indent-tabs-mode t)))
+                                (setq-local indent-tabs-mode t)))
 
 (when (executable-find "clangd")
   (add-hook 'c-mode-hook 'eglot-ensure))
