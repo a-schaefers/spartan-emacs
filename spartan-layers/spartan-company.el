2e55fa6c5b8777aec24a5daf6255ffed33bf7483
Adam Schaefers <schaefers@riseup.net>
2023-09-23 14:06:38 -0600

    Remove orderless fuzzy matching, place complete back on TAB

--- spartan-layers/spartan-company.el
+++ spartan-layers/spartan-company.el
@@ -12,7 +12,7 @@
   (setq company-idle-delay 0)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  ;; (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
   (define-key company-search-map (kbd "C-n") 'company-select-next)
   (define-key company-search-map (kbd "C-p") 'company-select-previous))
 
--- spartan-layers/spartan-vertico.el
+++ spartan-layers/spartan-vertico.el
@@ -44,15 +44,4 @@
   ;; Enable recursive minibuffers
   (setq enable-recursive-minibuffers t))
 
-(use-package orderless
-  :straight t
-  :defer t
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
 (provide 'spartan-vertico)
