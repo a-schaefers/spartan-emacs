6a17dfe445a1fef758d70e003889fdabe4419975
Adam Schaefers <schaefers@riseup.net>
2023-12-23 23:25:02 -0700

    [spartan-lua.el] use lua-mode for .rockspec files

--- spartan-library/spartan-lua.el
+++ spartan-library/spartan-lua.el
@@ -8,6 +8,7 @@
   :init
   (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
   (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+  (add-to-list 'auto-mode-alist '("\\.rockspec$" . lua-mode))
   (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
   :config
   (when (executable-find "lua-language-server")
