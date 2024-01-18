5ac7f82f627c4011baeb8d2bf8d96c2a43e4584d
Adam Schaefers <aesdef@riseup.net>
2024-01-17 17:42:14 -0700

    Introduce some added terminal-friendly binds for crux

--- spartan-layers/spartan-crux.el
+++ spartan-layers/spartan-crux.el
@@ -5,10 +5,18 @@
   :defer t
   :init
   (global-set-key (kbd "C-a") 'crux-move-beginning-of-line)
+  (global-set-key (kbd "C-x ;") 'comment-line) ; "C-x C-;" is interpreted this way in some terminals
   (global-set-key (kbd "C-o") 'crux-smart-open-line)
-  (global-set-key (kbd "C-<tab>") 'crux-other-window-or-switch-buffer)
+
+  (global-set-key (kbd "C-x C-o") 'crux-other-window-or-switch-buffer)
+  (global-set-key (kbd "C-c C-o") 'crux-other-window-or-switch-buffer)
+
   (global-set-key (kbd "C-c C-l") 'crux-duplicate-current-line-or-region)
+  (global-set-key (kbd "C-c l") 'crux-duplicate-current-line-or-region)
+
+  (global-set-key (kbd "C-c -") 'crux-kill-whole-line)
   (global-set-key (kbd "C-c C--") 'crux-kill-whole-line)
+
   (global-set-key (kbd "C-c ;") 'crux-duplicate-and-comment-current-line-or-region))
 
 (provide 'spartan-crux)
