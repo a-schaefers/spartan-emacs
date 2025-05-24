b8b6263a15ab294c0a5531c1b4d19f2cd965e110
Adam Schaefers <schaefers@riseup.net>
2025-05-24 04:08:51 -0600

    Fix formatting

--- init.el
+++ init.el
@@ -224,20 +224,20 @@
   (paredit
    :defer t
    :init
-   ((add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+   ((add-hook 'emacs-lisp-mode-hook        #'enable-paredit-mode)
    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-   (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+   (add-hook 'ielm-mode-hook               #'enable-paredit-mode)
    ;; lisps
-   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+   (add-hook 'lisp-interaction-mode-hook   #'enable-paredit-mode)
+   (add-hook 'lisp-mode-hook               #'enable-paredit-mode)
    ;; schemes
-   (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+   (add-hook 'scheme-mode-hook             #'enable-paredit-mode)
    ;; clojure
    (with-eval-after-load 'clojure-mode
      (add-hook 'clojure-mode-hook          #'enable-paredit-mode))
    ;; racket
    (with-eval-after-load 'racket-mode
-     (add-hook 'racket-mode-hook          #'enable-paredit-mode))))
+     (add-hook 'racket-mode-hook           #'enable-paredit-mode))))
 
   ;; (clojure-mode :defer t)
   ;; (cider :defer t)
