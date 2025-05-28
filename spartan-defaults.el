788240ac09bc2732bf557f4c5b7d26a69a595fc3
J. Bromley <jay@appliedinvention.com>
2025-05-28 14:47:30 -0700

    Move default spartan.el into its own file
    Previously, if a spartan.el file did not exists, then init.el wrote a large
    string that contained spartan.el. However, since the code was in a string,
    this made it cumbersome to edit any changes in the default spartan.el. Moved
    the contents of the string to its own file spartan-defaults.el and then,
    if spartan.el does not exists, init.el will copy spartan-defaults.el to
    spartan.el.

---
+++ spartan-defaults.el
@@ -0,0 +1,298 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Spartan.el Emacs General Settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set your name, email address, and directory where your code resides.
+(setq user-full-name    "John Doe"
+      user-mail-address "john.doe@example.com"
+      magit-repository-directories '(("~/repos" . 1)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Appearance
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Note: Users who dislike this theme may want to remove this section,
+;; and see the example below in the spartan-pkg macro for how to install and use another theme entirely.
+
+(setq-default frame-title-format '("Emacs"))
+(blink-cursor-mode -1)
+(scroll-bar-mode -1)
+(fringe-mode -1)
+(menu-bar-mode 1)
+(tool-bar-mode -1)
+
+(setq nano-splash-timeout nil
+      nano-splash-timeout-sec 5)
+
+(add-to-list 'load-path (concat user-emacs-directory "nano-emacs"))
+
+(require 'nano-minimal)
+(require 'nano-splash)
+(set-face-attribute 'default nil
+                    :height 140
+                    :weight 'light
+                    :family "Monospace") ; If you have Roboto Mono you may want to unset this here as it was the font this theme was designed to use
+
+;; (nano-dark)
+(nano-light)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Load layers
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq spartan-layers '(
+                       spartan-better-defaults ; Based on technomancy's better defaults
+                       spartan-better-scratch  ; Persistent, unkillable org-mode scratch buffer
+                       spartan-vertico         ; Adds fancier minibuffer
+                       spartan-flymake         ; Configures flymake to be our linter
+                       spartan-magit           ; A frontend to git
+                       spartan-projectile      ; Git project awareness and find/grep tools
+                       spartan-eglot           ; Adds lsp support
+                       spartan-company         ; Adds autocompletion drop-down menu
+                       spartan-shell           ; Misc. configuration and improvement to shell-mode
+                       spartan-treesit         ; Turns on treesitter everywhere as much as possible
+                       ))
+
+(add-to-list 'load-path (concat user-emacs-directory "spartan-layers"))
+(dolist (layer spartan-layers)
+  (require layer))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Modes that will autostart the corresponding eglot LSP server if found on PATH
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq spartan-eglot-autostart-langs
+      '(
+        (c-ts-mode-hook . clangd)
+        (c++-ts-mode-hook . clangd)
+        (lua-ts-mode-hook . lua-language-server)
+        (python-ts-mode-hook . pylsp)
+        (go-ts-mode-hook . gopls)
+        (rust-ts-mode-hook . rust-analyzer)
+        (ruby-ts-mode-hook . solargraph)
+        (elixir-ts-mode-hook . (:override elixir-ls))
+        (html-ts-mode-hook . vscode-html-language-server)
+        (css-ts-mode-hook . vscode-css-language-server)
+        (typescript-ts-mode-hook . typescript-language-server)
+        (js-ts-mode-hook . typescript-language-server)
+        (yaml-ts-mode-hook . yaml-language-server)
+        (json-ts-mode-hook . vscode-json-languageserver)
+        (java-ts-mode-hook . jdtls)
+        (csharp-ts-mode-hook . OmniSharp)
+
+        ;; (markdown-mode-hook . marksman)
+        ;; (php-mode-hook . true)          ; workaround, php lang server is not available on PATH but via required lib
+        ;; (zig-mode-hook . zigls)
+        ;; (terraform-mode-hook . terraform-ls)
+        ;; (nix-mode-hook . rnix-lsp)
+        ;; (haskell-mode-hook . haskell-language-server-wrapper)
+        ;; (ocaml-mode-hook . ocaml-lsp)
+        ;; (scala-mode-hook . metals)
+        ;; (forth-mode-hook . forth-lsp)
+        ;; (erlang-mode-hook . erlang_ls)
+        ;; (racket-mode-hook . true)       ; workaround, racket lang server is not available on PATH but via required lib
+        ;; (clojure-mode-hook . clojure-lsp)
+        ))
+
+;; iterate key value list of mode hooks and lsp bins and eglot-ensure
+;; Supports overriding lsp-bin's as follows, e.g.:
+;; (elixir-ts-mode-hook . (:override ("elixir-ls" "--stdio")))
+;;  (rust-ts-mode-hook . (:override rust-analyzer))
+(dolist (pair spartan-eglot-autostart-langs)
+  (let* ((hook (car pair))
+         (val (cdr pair))
+         (mode (intern (string-remove-suffix "-hook" (symbol-name hook))))
+         (override (and (consp val) (eq (car val) :override)))
+         (lsp-bin (if override (cadr val) val))
+         (cmd (cond
+               ((symbolp lsp-bin) (list (symbol-name lsp-bin)))
+               ((listp lsp-bin) lsp-bin)
+               (t nil))))
+    (when (and cmd (executable-find (car cmd)))
+      (add-hook hook #'eglot-ensure))
+    (when override
+      (eval-after-load 'eglot
+        `(add-to-list 'eglot-server-programs
+                      '(,mode . ,cmd))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Eglot LSP and Company binds
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(with-eval-after-load 'eglot
+  (define-key eglot-mode-map (kbd "M-m r") 'eglot-rename)
+  (define-key eglot-mode-map (kbd "M-m o") 'eglot-code-action-organize-imports)
+  (define-key eglot-mode-map (kbd "M-m h") 'eldoc)
+  (define-key eglot-mode-map (kbd "M-m =") 'eglot-format)
+  (define-key eglot-mode-map (kbd "M-m ?") 'xref-find-references)
+  (define-key eglot-mode-map (kbd "M-.")   'xref-find-definitions))
+
+;; Auto-completion bindings
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install and configure additional packages, this macro supports :defer :bind :config :init
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(spartan-pkg
+ ;; Original spartan-emacs theme, left in-place for those who may dislike the nano theme,
+ ;; or want an example of how to install and customize with another theme
+ ;;  (modus-themes :config
+ ;;                ((load-theme 'modus-vivendi t)
+
+ ;;                 ;; Set Font and Font Size here
+ ;;                 (set-face-attribute 'default nil :family \"Monospace\" :height 180)
+
+ ;;                 ;; Clean look
+ ;;                 (blink-cursor-mode -1)
+ ;;                 (scroll-bar-mode -1)
+ ;;                 (fringe-mode -1)
+ ;;                 (menu-bar-mode -1)
+ ;;                 (tool-bar-mode -1)
+
+ ;;                 ;; Remove hostname from the GUI titlebar
+ ;;                 (setq-default frame-title-format '(\"Emacs\"))
+
+ ;;                 ;; Clean mode-line
+
+ ;;                 ;; https://emacs.stackexchange.com/questions/5529/how-to-right-align-some-items-in-the-modeline
+ ;;                 (defun simple-mode-line-render (left right)
+ ;;                   \"Return a string of `window-width' length containing LEFT, and RIGHT
+ ;; aligned respectively.\"
+ ;;                   (let* ((available-width (- (window-width) (length left) 2)))
+ ;;                     (format (format \" %%s %%%ds \" available-width) left right)))
+
+ ;;                 (progn
+ ;;                   (setq-default mode-line-format
+ ;;                                 '((:eval (simple-mode-line-render
+ ;;                                           ;; left
+ ;;                                           (format-mode-line \"%* %b %l\")
+ ;;                                           ;; right
+ ;;                                           (format-mode-line \"%m\"))))))))
+
+  ;; Extensible vi layer
+  ;; (evil :config ((evil-mode 1)))
+
+  ;; Collection of Ridiculously Useful eXtensions
+  (crux :defer t :init
+        ((global-set-key (kbd "C-a") 'crux-move-beginning-of-line)
+        (global-set-key (kbd "C-o") 'crux-smart-open-line)
+        (global-set-key (kbd "C-x C-o") 'crux-other-window-or-switch-buffer)
+        (global-set-key (kbd "C-c C-l") 'crux-duplicate-current-line-or-region)
+        (global-set-key (kbd "C-c C--") 'crux-kill-whole-line)
+        (global-set-key (kbd "C-c ;") 'crux-duplicate-and-comment-current-line-or-region)))
+
+  ;; Additional langs that aren't supported OOTB yet by treesitter
+
+  ;; (markdown-mode :defer t)
+  ;; (php-mode :defer t)
+  ;; (haskell-mode :defer t)
+  ;; (zig-mode :defer t)
+  ;; (terraform-mode :defer t)
+  ;; (nix-mode :defer t )
+  ;; (systemd-mode :defer t)
+  ;; (dockerfile-mode :defer t)
+  ;; (nginx-mode :defer t)
+  ;; (tuareg-mode :defer t) ; ocaml
+  ;; (forth-mode :defer t)
+  ;; (erlang :defer t)
+  ;; (scala-mode :defer t)
+
+  ;;;; LISP general
+  (paredit
+   :defer t
+   :init
+   ((add-hook 'emacs-lisp-mode-hook        #'enable-paredit-mode)
+   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+   (add-hook 'ielm-mode-hook               #'enable-paredit-mode)
+   ;; lisps
+   (add-hook 'lisp-interaction-mode-hook   #'enable-paredit-mode)
+   (add-hook 'lisp-mode-hook               #'enable-paredit-mode)
+   ;; schemes
+   (add-hook 'scheme-mode-hook             #'enable-paredit-mode)
+   ;; clojure
+   (with-eval-after-load 'clojure-mode
+     (add-hook 'clojure-mode-hook          #'enable-paredit-mode))
+   ;; racket
+   (with-eval-after-load 'racket-mode
+     (add-hook 'racket-mode-hook           #'enable-paredit-mode))))
+
+  ;; (clojure-mode :defer t)
+  ;; (cider :defer t)
+
+  ;; (slime :defer t :init
+  ;;        ((setq inferior-lisp-program \"sbcl\")
+  ;;         (add-to-list 'auto-mode-alist '(\"\\\\.cl\\\\'\" . lisp-mode))
+  ;;         (add-to-list 'auto-mode-alist '(\"\\\\.sbclrc\\\\'\" . lisp-mode))))
+
+  ;; (racket-mode :defer t)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Additional config
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq-default
+ ;; these settings still should be set on a per language basis, this is just a general default
+ indent-tabs-mode nil ; In general, we prefer spaces
+ fill-column 79       ; python friendly
+ )
+
+;; C and C++ specific overrides (A language-specific override example)
+
+(defun spartan-c-ts-modes ()
+  ;; Use Linux kernel coding style in C and C++ (Tree-sitter modes)
+  ;; https://www.kernel.org/doc/html/v4.10/process/coding-style.html
+  (setq-local indent-tabs-mode t)               ; Use tabs
+  (setq-local tab-width 8)                      ; Display width of tab
+  ;; C-specific
+  (setq-local c-ts-mode-indent-style 'linux)
+  (setq-local c-ts-mode-indent-offset 8)
+  ;; C++-specific
+  (setq-local c++-ts-mode-indent-style 'linux)
+  (setq-local c++-ts-mode-indent-offset 8))
+
+(add-hook 'c-ts-mode-hook #'spartan-c-ts-modes)
+(add-hook 'c++-ts-mode-hook #'spartan-c-ts-modes)
+
+;; tabs are tabs in C family langs
+(add-hook 'makefile-mode-hook (lambda ()
+                                (setq-local indent-tabs-mode t)))
+
+;; Set default compile command, for make or whatever.
+(setq compile-command "make -k ")
+;; M-x cc
+(defalias 'cc 'compile)
+
+;; M-x sh
+(defalias 'sh 'better-shell-for-current-dir)
+
+;; M-x lint
+(defalias 'lint 'spartan-lint)
+
+;; M-x git
+(defalias 'git 'magit)
+
+;; M-x pro
+(defalias 'pro 'projectile-commander)
+
+;; Start the Emacs server for use by emacsclient
+(add-hook 'after-init-hook #'(lambda ()
+                               (interactive)
+                               (require 'server)
+                               (or (server-running-p)
+                                   (server-start))))
+
+;; Set EDITOR to emacsclient
+(or (getenv "EDITOR")
+    (progn
+      (setenv "EDITOR" "emacsclient")
+      (setenv "VISUAL" (getenv "EDITOR"))))
+
+;; Set PAGER to cat, for proper viewing of man pages, etc. while in M-x shell
+(or (getenv "PAGER")
+    (setenv "PAGER" "cat"))
