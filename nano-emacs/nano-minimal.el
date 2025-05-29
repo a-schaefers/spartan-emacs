e28caad4b97413c375fadf73f93e9455acb92743
Adam Schaefers <schaefers@riseup.net>
2025-05-29 10:25:02 -0600

    Use internal-border-width . 16 to improve screen real estate

--- nano-emacs/nano-minimal.el
+++ nano-emacs/nano-minimal.el
@@ -22,7 +22,7 @@
 ;; --- Frame / windows layout & behavior --------------------------------------
 (setq default-frame-alist
       '((height . 44) (width  . 81) (left-fringe . 0) (right-fringe . 0)
-        (internal-border-width . 32) (vertical-scroll-bars . nil)
+        (internal-border-width . 16) (vertical-scroll-bars . nil)
         (bottom-divider-width . 0) (right-divider-width . 0)
         (undecorated-round . nil)))
 (modify-frame-parameters nil default-frame-alist)
